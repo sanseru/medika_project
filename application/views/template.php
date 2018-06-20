9807d98e769f6e9ac25a2119557ed514ad0fe2e3
sanseru <sanseru1@yahoo.com>
2018-06-20 11:46:02 +0700

    v.3.2

--- application/views/template.php
+++ application/views/template.php
@@ -47,9 +47,9 @@
                 <!-- Logo -->
                 <a href="<?php echo base_url() ?>adminlte/index2.html" class="logo">
                     <!-- mini logo for sidebar mini 50x50 pixels -->
-                    <span class="logo-mini"><b>MEDIKA</b></span>
+                    <span class="logo-mini"><b>MP</b></span>
                     <!-- logo for regular state and mobile devices -->
-                    <span class="logo-lg"><b>PLAZA</b></span>
+                    <span class="logo-lg"><b>+</b></span>
                 </a>
                 <!-- Header Navbar: style can be found in header.less -->
                 <nav class="navbar navbar-static-top">
