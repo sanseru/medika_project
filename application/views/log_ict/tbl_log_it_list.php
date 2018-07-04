3747d9c91ac954dc153d0b0619abd9db32eee934
sanseru <sanseru1@yahoo.com>
2018-07-04 15:52:47 +0700

    7-4-2018_1

--- application/views/log_ict/tbl_log_it_list.php
+++ application/views/log_ict/tbl_log_it_list.php
@@ -5,7 +5,7 @@
                 <div class="box box-warning box-solid">
     
                     <div class="box-header">
-                        <h3 class="box-title">KELOLA DATA TBL_LOG_IT</h3>
+                        <h3 class="box-title">LOG ICT</h3>
                     </div>
         
         <div class="box-body">
